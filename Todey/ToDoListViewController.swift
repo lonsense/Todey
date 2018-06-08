9db24d76d77777db8d1cba6fbf29a04ed64d5ddc
Michael Garden <michael.garden.nz@gmail.com>
2018-06-08 12:16:09 +1200

    1. Created the ability to add to the itemArray

--- Todey/ToDoListViewController.swift
+++ Todey/ToDoListViewController.swift
@@ -11,26 +11,25 @@ import UIKit
 class ToDoListViewController: UITableViewController {
 
     
-    let itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    //Determine the number of rows in the table
+    //MARK - Determine the number of rows in the table
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let numberOfRows : Int = itemArray.count
         return numberOfRows
     }
     
-    //What the cells should display
+    //MARK - What the cells should display
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         cell.textLabel?.text =  itemArray[indexPath.row]
         return cell
-    
     }
    
     //MARK - Tableview Delegate methods (when you click on any part of the table)
@@ -52,7 +51,36 @@ class ToDoListViewController: UITableViewController {
        
     
     }
-    
+    //MARK - Add item to list
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        
+        let alert = UIAlertController(title: "Add new list item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+        
+            
+            //What happens when the user clicks the add item button on our UI Alert
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+            
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+                
+
+            
+           
+        
+        }
+        
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+      
+    }
     
 
 }
